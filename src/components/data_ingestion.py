dd900f667d477be90bf2bca71ef7258209dd4485
Vedant mahida <vedantmahida.science@gmail.com>
2024-03-02 15:43:46 -0500

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,68 @@
+"""
+src/components/data_ingestion.py
+"""
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionConfig:
+    """
+    Configuration class for data ingestion.
+    """
+    raw_data_path: str = os.path.join('artifacts', 'raw_data.csv')
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        """
+        Method to initiate the data ingestion process.
+
+        Reads the dataset, saves the raw data to artifacts, performs 
+        train-test split, and saves the split datasets to artifacts.
+        """
+        logging.info("Entered data ingestion method of component.")
+        try:
+            # read dataset and save raw data to artifacts
+            df = pd.read_csv('notebook/data/StudentsPerformance.csv')
+            logging.info("Read the dataset as dataframe.")
+
+            os.makedirs(os.path.dirname(
+                self.ingestion_config.train_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,
+                      index=False, header=True)
+            logging.info("Train test split initiated.")
+
+            # train test datasets and save to artifacts
+            train_set, test_set = train_test_split(
+                df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(
+                self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(
+                self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Data ingestion is completed.")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+if __name__ == "__main__":
+    ingestion = DataIngestion()
+    ingestion.initiate_data_ingestion()
